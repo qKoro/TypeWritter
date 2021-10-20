44d14cee092d27d3e8a1b1288e7e106d0ae36bab
qKoro <87548940+qKoro@users.noreply.github.com>
2021-10-20 21:23:13 +0200

    Add files via upload

---
+++ exemple.py
@@ -0,0 +1,5 @@
+import typewriter #import typewriter
+
+message = "Hey, I'm qKoro. I speak French and I love coding <3\nCordially" #<-- create a variable with your message #write delay (0.05 second) paragraph delay (0.17 second) 
+
+typewriter.typewriter(message) #<-- Write the message
